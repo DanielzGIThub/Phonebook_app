8d5fb38f4a5fa6f59b0d6610f97822aac1884421
DanielzGIThub <dzmsgbox@gmail.com>
2019-09-13 20:53:32 +0200

    Amendments to code and files

--- phbook/urls.py
+++ phbook/urls.py
@@ -3,14 +3,11 @@ from . import views
 
 
 urlpatterns = [
-    path('index/', views.personlist1, name='index'),
-    path('addperson/', views.add_person, name='addperson'),
+    path('index/', views.personslist, name='index'),
+    path('search/', views.SearchByPerson.as_view(), name='search_results'),
+    path('addperson/', views.add_person, name='add_person'),
     path('editperson/<int:id>/', views.edit_person, name='edit_person'),
     path('delperson/<int:id>/', views.del_person, name='del_person'),
-    path('response/', views.del_person, name='response'),
-    path('search/', views.SearchByPerson.as_view(), name='search_results'),
-    path('addphone/<int:id>/', views.phoneadd, name='add_phone'),
-    path('addemail/<int:id>/', views.emailadd, name='add_email'),
-    path('testphone/', views.phonesave, name='testphone'),
-    path('testemail/', views.emailsave, name='testemail'),
+    path('addphone/<int:id>/', views.add_phone, name='add_phone'),
+    path('addemail/<int:id>/', views.add_email, name='add_email'),
 ]
--- phbook/views.py
+++ phbook/views.py
@@ -1,102 +1,71 @@
-from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404, redirect
 from .models import Person, Email, Phonenumber
 from django.views import generic
 from django.db.models import Q
 from django.views.decorators.csrf import csrf_exempt
 from .forms import PersonAdd
-from django.contrib.auth.decorators import login_required
 
 
-def personlist1(request):
+def personslist(request):
     allpersons = Person.objects.all().order_by('lastname')
-    x=True
-    context = {'allpersons': allpersons, 'x': x}
-    
-
-    return render(request, 'index.html', context)
+    return render(request, 'index.html', {'allpersons': allpersons})
 
 
 class SearchByPerson(generic.ListView):
     model = Person
     template_name = 'searchresults.html'
     context_object_name = 'allpersons'
-
     def get_queryset(self): 
         query = self.request.GET.get('q')
         object_list = Person.objects.filter(
-            Q(firstname__icontains=query) | Q(lastname__icontains=query) | Q(email__email__contains=query) | Q(phonenumber__phonenumber__contains=query)
-        ).distinct()
+            Q(firstname__icontains=query) | Q(lastname__icontains=query) 
+            | Q(email__email__contains=query) | Q(phonenumber__phonenumber__contains=query)
+            ).distinct().order_by('lastname')
         return object_list
 
 
-#@login_required
 def add_person(request):
     form = PersonAdd(request.POST or None)
     if form.is_valid():
         form.save()
-        return redirect(personlist1)
-    return render(request, 'new_person.html', {'form' : form})
+        return redirect(personslist)
+    return render(request, 'addperson.html', {'form' : form})
 
 
-#@login_required
 def edit_person(request, id):
     person = get_object_or_404(Person, pk=id)
     form = PersonAdd(request.POST or None, instance=person)
     if form.is_valid():
         form.save()
-        return redirect(personlist1)
-    return render(request, 'new_person.html', {'form' : form})
+        return redirect(personslist)
+    return render(request, 'addperson.html', {'form' : form})
 
 
-#@login_required
 def del_person(request, id):
     person = get_object_or_404(Person, pk=id)
-    pname =str(person.firstname) + ' ' + str(person.lastname)
-    if len(person.email_set.all())==0 and len(person.phonenumber_set.all())==0:
-        if request.method == 'POST':
-            person.delete()
-            return redirect(personlist1)
-    else:
-        return render(request, 'responsepage.html', {'pname': pname})
-    
-    return render(request, 'confirm.html', {'person' : person, 'pname': pname})
+    if request.method == 'POST':
+        person.delete()
+        return redirect(personslist)
+    return render(request, 'delconfirm.html', {'person' : person})
 
 
 @csrf_exempt
-def email(request):
-    #id = request.POST.get('id')
-    #return render(request, 'test.html', {'id': id})
-    id = request.POST.get('id')
-    email = request.POST.get('email')
-    person = Person.objects.get(pk=id)
-    person.email_set.create(email=email)
-    return redirect(personlist1)
-
-
-#@login_required
-def phoneadd(request, id):
+def add_phone(request, id):
+    if request.method == 'POST':
+        id = request.POST.get('id')
+        phone = request.POST.get('phone')
+        person = Person.objects.get(pk=id)
+        person.phonenumber_set.create(phonenumber=phone)
+        return redirect(personslist)
     return render(request, 'addphone.html', {'id' : id})
 
 
 @csrf_exempt
-def phonesave(request):
-    id = request.POST.get('id')
-    phone = request.POST.get('phone')
-    person = Person.objects.get(pk=id)
-    person.phonenumber_set.create(phonenumber=phone)
-    return redirect(personlist1)
-
-
-#@login_required
-def emailadd(request, id):
+def add_email(request, id):
+    if request.method == 'POST':
+        id = request.POST.get('id')
+        email = request.POST.get('email')
+        person = Person.objects.get(pk=id)
+        person.email_set.create(email=email)
+        return redirect(personslist)
     return render(request, 'addemail.html', {'id' : id})
-
-
-@csrf_exempt
-def emailsave(request):
-    id = request.POST.get('id')
-    email = request.POST.get('email')
-    person = Person.objects.get(pk=id)
-    person.email_set.create(email=email)
-    return redirect(personlist1)
